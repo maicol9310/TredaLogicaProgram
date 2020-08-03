9ec2d7d08df9e841d0379273e2dd1e562e285e7e
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2020-08-02 23:15:46 -0500

    Metodo CamelCase implementado

---
+++ TredaCamelCase/TredaCamelCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TredaCamelCase
+{
+    class Program
+    {
+        public static string camelcase(string camel)
+        {
+            camel = camel.Replace(" ", "").Replace("_", "").Replace("-", "");
+            return camel;
+        }
+
+        public static void Main(string[] args)
+        {
+            string N;
+            string Resultado;
+            Console.WriteLine("Ingresar text:");
+            N = Console.ReadLine();
+
+            Resultado = camelcase(N);
+
+
+            Console.WriteLine(Resultado);
+            Console.ReadLine();
+        }
+    }
+}
