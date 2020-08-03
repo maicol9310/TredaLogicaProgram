020523be316a0242a1607d767d251b2d2d9ba9a8
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2020-08-02 23:29:53 -0500

    Cadena invertida

---
+++ Invertir/Invertir/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertir
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string N;
+            Console.WriteLine("Ingresa texto a invertir:");
+            N = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Texto Normal: "+N);
+            Console.WriteLine("Texto Invertido: "+Invertir(N));
+            Console.ReadLine();
+        }
+
+        public static string Invertir(string cadena)
+        {
+            char[] cadenaInver = cadena.ToCharArray();
+            Array.Reverse(cadenaInver);
+            return new string(cadenaInver);
+        }
+    }
+}
